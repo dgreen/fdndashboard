e36ebe12de71f25df49a3ede477bfcea95f70a3d
David Green <dgreen@uab.edu>
2019-05-01 14:25:36 -0500

    Add configuration file and connect calendar widget

---
+++ initData.js
@@ -0,0 +1,17 @@
+// -------------- Edit values:
+var capcBudget    = 3941.
+var capcValue     = 1500
+
+var psvbBudget    = 5013.
+var psvbValue     = 2000
+
+var afrvbBudget   = 2607
+var afrvbValue    = 700
+
+var deDate        = '3/31/2019'
+
+var niiBudget     = 2278
+var niiValue      = 700
+
+var pterValue     = 84
+// -------------- End editing
