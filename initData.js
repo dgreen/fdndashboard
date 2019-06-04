0b8dbb654e0a63e0389d50ea35cfd27e752c5289
David Green <dgreen@uab.edu>
2019-06-03 20:34:04 -0500

    Add text to NII display with substitution, clean up

--- initData.js
+++ initData.js
@@ -1,17 +1,19 @@
 // -------------- Edit values:
-var capcBudget    = 3941.
-var capcValue     = 1500
+var capcBudget     = 4000
+var capcValue      = 1500
 
-var psvbBudget    = 5013.
-var psvbValue     = 2000
+var psvbBudget     = 4000
+var psvbValue      = 2000
 
-var afrvbBudget   = 2607
-var afrvbValue    = 700
+var afrvbBudget    = 4000
+var afrvbValue     = 700
 
-var deDate        = '3/31/2019'
+var deDate         = '3/31/2019'
 
-var niiBudget     = 2278
-var niiValue      = 700
+var niiBudget      = 1500
+var niiValue       = 750
+var niiPercentYTD  = 3
+var niiPercentGoal = 6
 
-var pterValue     = 84
+var pterValue      = 84
 // -------------- End editing
